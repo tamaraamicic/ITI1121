c17303711555ca4ae7df65426574e0ca63b8fd9e
tamaraamicic <78327636+tamaraamicic@users.noreply.github.com>
2021-04-02 21:02:56 -0400

    Friday Night Fun

--- DecisionTree.java
+++ DecisionTree.java
@@ -51,10 +51,16 @@ public class DecisionTree {
 		}
 
 		// BASE CASES 
+
+		//2.1
 		if (node.data.getNumberOfAttributes() == 1 && node.data.getAttribute(0).equals(node.data.getSourceDataSet().attributes[-1])) {
-
-		}else if(node.data.getUniqueAttributeValues(0).length == 1){
-
+			//????
+		//2.2
+		} else if(node.data.getUniqueAttributeValues(0).length == 1){
+			//return node.data.getUniqueAttributeValues(0)[0];
+		//2.3
+		} else if (noAttributeHasMoreThanOneUniqueValue(node.data)) {
+			//return something NO
 		}
 		
 		// RECURSIVE CASE
@@ -75,25 +81,39 @@ public class DecisionTree {
 		}
 
 		//3.2
-
-		int split = Integer.parseInt(splitString);
+		
+		VirtualDataSet[] partitions;
 
 		if (a_max.getType() == AttributeType.NOMINAL){
-			VirtualDataSet[] partitions = node.data.partitionByNominallAttribute(a_max.getAbsoluteIndex());
+			partitions = node.data.partitionByNominallAttribute(a_max.getAbsoluteIndex());
 		}else{
-			VirtualDataSet[] partitions = node.data.partitionByNumericAttribute(a_max.getAbsoluteIndex(), split);
+			int split = Integer.parseInt(splitString);
+			partitions = node.data.partitionByNumericAttribute(a_max.getAbsoluteIndex(), split);
 		}	
 
 		//3.3
+		node.children = (Node<VirtualDataSet>[]) new Node[partitions.length];
 
+		//3.4 and 3.5
+		for (int i = 0; i < partitions.length; i++) {
+			node.children[i] = new Node(partitions[i]);
+			build(node.children[i]);
+		}
 
+	}
 
+	private boolean noAttributeHasMoreThanOneUniqueValue(VirtualDataSet data) {
+		for (int i = 0; i < data.getNumberOfAttributes() - 1; i++) {
+			if (data.getUniqueAttributeValues(i).length > 1) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public String toString() {
 		return toString(root, 0);
-		// fun 
 	}
 
 	/**
@@ -107,9 +127,12 @@ public class DecisionTree {
 	 */
 	private String toString(Node<VirtualDataSet> node, int indentDepth) {
 		// WRITE YOUR CODE HERE!
+		String output = "if (" + node.data + ") is \'" + node.children[0] + "\') {" + createIndent(indentDepth) 
+		+ "\n"+ toString(node.children[0], indentDepth + 1) + "\n"+ createIndent(indentDepth) + "class = " + 
+		node.data.getUniqueAttributeValues(0)[0] + "}";
 		
 		// Remove the following line once you have implemented the method
-		return null;
+		return output;
 	}
 
 	/**
@@ -133,18 +156,35 @@ public class DecisionTree {
 	
 		StudentInfo.display();
 
-		if (args == null || args.length == 0) {
-			System.out.println("Expected a file name as argument!");
-			System.out.println("Usage: java DecisionTree <file name>");
-			return;
-		}
+		// if (args == null || args.length == 0) {
+		// 	System.out.println("Expected a file name as argument!");
+		// 	System.out.println("Usage: java DecisionTree <file name>");
+		// 	return;
+		// }
 
-		String strFilename = args[0];
+		// String strFilename = args[0];
 
-		ActualDataSet data = new ActualDataSet(new CSVReader(strFilename));
+		// ActualDataSet data = new ActualDataSet(new CSVReader(strFilename));
 
-		DecisionTree dtree = new DecisionTree(data);
+		// DecisionTree dtree = new DecisionTree(data);
 
-		System.out.println(dtree);
+		// System.out.println(dtree);
+
+		ActualDataSet data1 = new ActualDataSet(new CSVReader("weather-nominal.csv"));
+		DecisionTree dtree1 = new DecisionTree(data1);
+
+		System.out.println("*** Decision tree for weather-nominal.csv ***");
+		System.out.println();
+
+		System.out.println(dtree1);
+
+		System.out.println("*** Decision tree for weather-numeric.csv ***");
+		System.out.println();
+
+		ActualDataSet data2 = new ActualDataSet(new CSVReader("weather-numeric.csv"));
+		DecisionTree dtree2 = new DecisionTree(data2);
+
+		System.out.println(dtree2);
+
 	}
 }
