a3e81e3481f13fc8a8dc3fd96eb3f9c0c5c256dc
Oleksander Turchyn <52382133+OlesTurchyn@users.noreply.github.com>
2021-04-05 15:31:21 -0400

    Monday Excellence

--- DecisionTree.java
+++ DecisionTree.java
@@ -36,8 +36,8 @@ public class DecisionTree {
 	@SuppressWarnings("unchecked")
 	private void build(Node<VirtualDataSet> node) {
 		// WRITE YOUR CODE HERE!
-		System.out.println();
-		System.out.println("Method call");
+		//System.out.println();
+		//System.out.println("-----METHOD CALL-----");
 
 		// EDGE CASES ??
 		if (node == null || node.data == null){
@@ -53,22 +53,21 @@ public class DecisionTree {
 		}
 
 		// BASE CASES 
-
-		System.out.println("INDEX OF CLASS: " + (node.data.getSourceDataSet().attributes.length-1));
+		//System.out.println("node.data: " + node.data);
 		//2.1
 		if (node.data.getNumberOfAttributes() == 1 && node.data.getAttribute(0).equals
 			(node.data.getSourceDataSet().attributes[node.data.getSourceDataSet().attributes.length-1])) {
-			System.out.println("base case 1");
+			//System.out.println("base case 1");
 			return;
 
 		//2.2
 		} else if(node.data.getUniqueAttributeValues(node.data.attributes.length-1).length == 1){
-			System.out.println("base case 2");
+			//System.out.println("base case 2");
 			return;
 
 		//2.3
 		} else if (noAttributeHasMoreThanOneUniqueValue(node.data)) {
-			System.out.println("base case 3");
+			//System.out.println("base case 3");
 			return;
 		}
 		
@@ -79,51 +78,31 @@ public class DecisionTree {
 
 		Attribute a_max = node.data.getAttribute(gains[0].getAttributeName()); 
 		String splitString = gains[0].getSplitAt();
+
+		// System.out.println("splitString: " + splitString);
+
+		// System.out.println("a_max: " + a_max.getName());
 		
-		// for (int i = 0; i < node.data.getNumberOfAttributes(); i++) {
-		// 	System.out.println("attributes: " + node.data.getAttribute(i));
-
-		// } 
-		// System.out.println();
-
-		// double maxGainValue = gains[0].getGainValue();
-	
-		// for (int i = 0; i < gains.length; i++){
-		// 	//System.out.println("mango");
-		// 	System.out.println("gain: " + gains[i] + " gain value: " + gains[i].getGainValue());
-		// 	System.out.println("maxGainValue: " + maxGainValue);
-		// 	if (gains[i].getGainValue() > maxGainValue) {
-		// 		System.out.println("apple");
-		// 		maxGainValue = gains[i].getGainValue();
-		// 		System.out.println("gains[i].getAttributeName(): " +  gains[i].getAttributeName());
-		// 		a_max = node.data.getAttribute(gains[i].getAttributeName());
-		// 		splitString = gains[i].getSplitAt();
-		// 	}
-		// }
-
-		System.out.println("a_max: " + a_max.getName());
-		// System.out.println();
-
+		
 		//3.2
-		
 		VirtualDataSet[] partitions;
 
 		if (a_max.getType() == AttributeType.NOMINAL){
-			//System.out.println("a_max: " + a_max.getName());
-			partitions = node.data.partitionByNominallAttribute(a_max.getAbsoluteIndex());
+			partitions = node.data.partitionByNominallAttribute(node.data.getAttributeIndex(a_max.getName()));
 		}else{
-			int split = Integer.parseInt(splitString);
-			//System.out.println("split: " + split);
-
-			//System.out.println("a_max: " + a_max.getName());
-			//System.out.println("a_max.getAbsoluteIndex(): " + a_max.getAbsoluteIndex());
-			partitions = node.data.partitionByNumericAttribute(a_max.getAbsoluteIndex(), split);
-		
+			//?????????????????
+			int splitIndex = 0;
+			for (int i = 0; i < node.data.getUniqueAttributeValues(a_max.getName()).length; i++){
+				if(node.data.getUniqueAttributeValues(a_max.getName())[i].equals(splitString)) {
+					splitIndex = i;
+				}
+			}
+			
+			partitions = node.data.partitionByNumericAttribute(node.data.getAttributeIndex(a_max.getName()), splitIndex);
+			 
 		}	
 
 		//3.3
-
-		//System.out.println("partitions.length: " + partitions.length);
 		node.children = (Node<VirtualDataSet>[]) new Node[partitions.length];
 
 		//3.4 and 3.5
@@ -159,14 +138,38 @@ public class DecisionTree {
 	 */
 	private String toString(Node<VirtualDataSet> node, int indentDepth) {
 		// WRITE YOUR CODE HERE!
-		//build(node);
 
-		// String output = "if (" + node.data.getAttribute(0) + ") is \'" + node.children[0] + "\') {" + createIndent(indentDepth) 
-		// + "\n"+ toString(node.children[0], indentDepth + 1) + "\n"+ createIndent(indentDepth) + "class = " + 
-		// node.data.getUniqueAttributeValues(0)[0] + "}";
+		StringBuffer buffer = new StringBuffer();
+
+		//base cases
 		
+		if (node.data.getNumberOfAttributes() == 1 && node.data.getAttribute(0).equals
+			(node.data.getSourceDataSet().attributes[node.data.getSourceDataSet().attributes.length-1])) {
+			return (createIndent(indentDepth) + node.data.getAttribute(node.data.attributes.length-1).getName() + " = " + node.data.getUniqueAttributeValues((node.data.attributes.length-1))[0] + "\n");
+		}
+		
+		if (node.data.getUniqueAttributeValues(node.data.attributes.length-1).length == 1){
+			return (createIndent(indentDepth) + node.data.getAttribute(node.data.attributes.length-1).getName() + " = " + node.data.getUniqueAttributeValues((node.data.attributes.length-1))[0]+ "\n");
+		}
+
+		if (noAttributeHasMoreThanOneUniqueValue(node.data)) {
+			return (createIndent(indentDepth) + node.data.getAttribute(node.data.attributes.length-1).getName() + " = " + node.data.getUniqueAttributeValues((node.data.attributes.length-1))[0]+ "\n");
+		}
+
+		//recursive case
+
+		for (int i = 0; i < node.children.length; i++) {
+			if (i == 0) {
+				buffer.append(createIndent(indentDepth) + "if (" + node.children[i].data.getCondition() + ") {" + "\n");
+			} else {
+				buffer.append(createIndent(indentDepth) + "else if (" + node.children[i].data.getCondition() + ") {" + "\n");
+			}
+			buffer.append(toString(node.children[i], indentDepth + 1));
+			buffer.append(createIndent(indentDepth) + "}" + "\n");
+		}
+
 		// Remove the following line once you have implemented the method
-		return "banana";
+		return buffer.toString();
 	}
 
 	/**
@@ -181,7 +184,7 @@ public class DecisionTree {
 		}
 		StringBuffer buffer = new StringBuffer();
 		for (int i = 0; i < indentDepth; i++) {
-			buffer.append(' ');
+			buffer.append("  ");
 		}
 		return buffer.toString();
 	}
